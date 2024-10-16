9eeeebb156a60c990533c5d28bcb71241a2d98d6
ARYAN THORAT <bussinesswitharyanthorat@gmail.com>
2024-10-16 11:37:51 +0530

    Add files via upload

---
+++ Linked list 18th practical.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node* next;
+Node(int data){
+        value=data;
+        next=NULL;
+    }
+};
+int main(){
+    Node* a=new Node(1);
+cout<<a->value<<" "<<a->next<<endl;
+}
+
+/*
+1 0
+*/
